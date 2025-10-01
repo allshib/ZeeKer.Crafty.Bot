0d9693a478d5d1fbd9c450f20c0721fce0ad43ae
Alex Shibalkin <al.shibalkin@gmail.com>
2025-10-01 12:14:21 +0500

    Добавление DockerFile

--- ZeeKer.Crafty/Configuration/TelegramBotOptions.cs
+++ ZeeKer.Crafty/Configuration/TelegramBotOptions.cs
@@ -7,9 +7,6 @@ public record class TelegramBotOptions
     [Required]
     public string Token { get; init; } = string.Empty;
 
-    [Required]
-    public string ChatId { get; init; } = string.Empty;
-
     [Range(1, int.MaxValue, ErrorMessage = "UpdateIntervalMinutes must be greater than zero.")]
     public int UpdateIntervalMinutes { get; init; } = 1;
 }
