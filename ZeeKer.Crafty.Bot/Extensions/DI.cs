30a6fe1cbc4d8d964419979cb20cc34a0962021b
Alex Shibalkin <al.shibalkin@gmail.com>
2025-10-01 11:30:24 +0500

    Исправление DI

---
+++ ZeeKer.Crafty.Bot/Extensions/DI.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Options;
+using Telegram.Bot;
+using ZeeKer.Crafty.Bot.Messaging;
+using ZeeKer.Crafty.Bot.Services;
+using ZeeKer.Crafty.Configuration;
+
+namespace ZeeKer.Crafty.Bot.Extensions;
+
+public static class DI
+{
+    public static IServiceCollection AddBotServices(this IServiceCollection services)
+    {
+        services.AddSingleton<ITelegramBotClient>(sp =>
+        {
+            var options = sp.GetRequiredService<IOptions<TelegramBotOptions>>().Value;
+            return new TelegramBotClient(options.Token);
+        });
+
+        services.AddSingleton<ITelegramNotifier, TelegramNotifier>();
+        services.AddSingleton<ServerStatisticsMessageBuilder>();
+        services.AddHostedService<CraftyStatusBroadcastService>();
+
+
+        return services;
+    }
+}
+
--- ZeeKer.Crafty.Bot/Program.cs
+++ ZeeKer.Crafty.Bot/Program.cs
@@ -1,55 +1,36 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Telegram.Bot;
-using ZeeKer.Crafty.Bot.Messaging;
-using ZeeKer.Crafty.Bot.Services;
 using ZeeKer.Crafty.Configuration;
 using ZeeKer.Crafty.Client;
 using ZeeKer.Crafty.Storage;
+using ZeeKer.Crafty.Bot.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
-
-// Swagger
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
 
 builder.Services.Configure<TelegramBotOptions>(
     builder.Configuration.GetSection("TelegramBot"));
 builder.Services.AddOptions<TelegramBotOptions>()
     .ValidateDataAnnotations();
 
-builder.Services.AddSingleton<ITelegramBotClient>(sp =>
-{
-    var options = sp.GetRequiredService<IOptions<TelegramBotOptions>>().Value;
-    return new TelegramBotClient(options.Token);
-});
 
-builder.Services.AddCraftyClient(builder.Configuration);
+builder.Services
+    .AddStorage(builder.Configuration, builder.Environment.ContentRootPath)
+    .AddBotServices()
+    .AddCraftyClient(builder.Configuration);
 
-builder.Services.AddSingleton<ITelegramNotifier, TelegramNotifier>();
-builder.Services.AddSingleton<ServerStatisticsMessageBuilder>();
-builder.Services.AddHostedService<CraftyStatusBroadcastService>();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-builder.Services.AddStorage(builder.Configuration, builder.Environment.ContentRootPath);
 
 var app = builder.Build();
 
 using var scope = app.Services.CreateScope();
 scope.ServiceProvider.Migrate();
 
-app.Services.GetRequiredService<ITelegramNotifier>();
-
 app.UseSwagger();
 app.UseSwaggerUI();
 
-
-// Configure the HTTP request pipeline.
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
